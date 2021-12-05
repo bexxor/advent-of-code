4f20c3d60104814ca2719574379bc98a40429585
bexxor <rebekka.lutolf@gmail.com>
2021-12-05 09:58:43 +0100

    solve day5 21

--- src/aoc21/Day05.java
+++ src/aoc21/Day05.java
@@ -3,29 +3,86 @@ package aoc21;
 import util.AoCDay;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day05 extends AoCDay {
 
-    private final List<String> inputList;
+	private static final int MAX = 1000;
+	private final int[][] diagram;
+	private List<int[]> lines;
 
-    public Day05(int day) {
-        super(day, 2021, 1);
-        this.inputList = super.getInputList();
-    }
+	public Day05(int day) {
+		super(day, 2021, 2);
+		this.diagram = new int[MAX][MAX];
+		this.lines = new ArrayList<>();
+		handleInput();
+	}
 
-    @Override
-    public void handleInput() {
-    }
+	@Override
+	public void handleInput() {
+		lines = getInputStream().map(l -> Arrays.stream(l.split(",|\s->\s")).mapToInt(Integer::parseInt).toArray()).collect(Collectors.toList());
+		for (int[] row : diagram) {
+			Arrays.fill(row, 0);
+		}
+	}
 
-    @Override
-    public String solve1() {
-        return String.valueOf(-1);
-    }
+	@Override
+	public String solve1() {
+		fillDiagram();
+		return String.valueOf(countOverlaps());
+	}
 
-    @Override
-    public String solve2() {
-        return String.valueOf(-1);
-    }
+	@Override
+	public String solve2() {
+		fillDiagram();
+		return String.valueOf(countOverlaps());
+	}
+
+	private void fillDiagram() {
+		for (int[] line : lines) {
+			int x1 = line[0];
+			int y1 = line[1];
+			int x2 = line[2];
+			int y2 = line[3];
+			if (x1 == x2) {
+				for (int y = Math.min(y1, y2), maxY = y == y1 ? y2 : y1; y <= maxY; y++) {
+					diagram[y][x1] += 1;
+				}
+			} else if (y1 == y2) {
+				for (int x = Math.min(x1, x2), maxX = x == x1 ? x2 : x1; x <= maxX; x++) {
+					diagram[y1][x] += 1;
+				}
+			} else if (part == 2) {
+				if (x1 <= x2 && y1 <= y2) {
+					for (int i = 0, diff = x2 - x1; i <= diff; i++) {
+						diagram[y1 + i][x1 + i] += 1;
+					}
+				} else if (x1 <= x2) {
+					for (int i = 0, diff = x2 - x1; i <= diff; i++) {
+						diagram[y1 - i][x1 + i] += 1;
+					}
+				} else if (y1 > y2) {
+					for (int i = 0, diff = x1 - x2; i <= diff; i++) {
+						diagram[y1 - i][x1 - i] += 1;
+					}
+				} else {
+					for (int i = 0, diff = x1 - x2; i <= diff; i++) {
+						diagram[y1 + i][x1 - i] += 1;
+					}
+				}
+			}
+		}
+	}
+
+	private int countOverlaps() {
+		int count = 0;
+		for (int[] line : diagram) {
+			for (int point : line) {
+				count += point >= 2 ? 1 : 0;
+			}
+		}
+		return count;
+	}
 }
--- src/util/GridUtil.java
+++ src/util/GridUtil.java
@@ -4,263 +4,269 @@ import java.util.Arrays;
 
 public class GridUtil {
 
-    public static int countNeighbours(int i, int j, String[][] array2d, String val) {
-        int count = 0;
-        if (i > 0) {
-            if (j > 0 && array2d[i - 1][j - 1].equals(val)) {
-                count++;
-            }
-            if (j < array2d[i].length - 1 && array2d[i - 1][j + 1].equals(val)) {
-                count++;
-            }
-            if (array2d[i - 1][j].equals(val)) {
-                count++;
-            }
-        }
-        if (i < array2d.length - 1) {
-            if (j > 0 && array2d[i + 1][j - 1].equals(val)) {
-                count++;
-            }
-            if (j < array2d[i].length - 1 && array2d[i + 1][j + 1].equals(val)) {
-                count++;
-            }
-            if (array2d[i + 1][j].equals(val)) {
-                count++;
-            }
-        }
-        if (j > 0 && array2d[i][j - 1].equals(val)) {
-            count++;
-        }
-        if (j < array2d[i].length - 1 && array2d[i][j + 1].equals(val)) {
-            count++;
-        }
-        return count;
-    }
+	public static int countNeighbours(int i, int j, String[][] array2d, String val) {
+		int count = 0;
+		if (i > 0) {
+			if (j > 0 && array2d[i - 1][j - 1].equals(val)) {
+				count++;
+			}
+			if (j < array2d[i].length - 1 && array2d[i - 1][j + 1].equals(val)) {
+				count++;
+			}
+			if (array2d[i - 1][j].equals(val)) {
+				count++;
+			}
+		}
+		if (i < array2d.length - 1) {
+			if (j > 0 && array2d[i + 1][j - 1].equals(val)) {
+				count++;
+			}
+			if (j < array2d[i].length - 1 && array2d[i + 1][j + 1].equals(val)) {
+				count++;
+			}
+			if (array2d[i + 1][j].equals(val)) {
+				count++;
+			}
+		}
+		if (j > 0 && array2d[i][j - 1].equals(val)) {
+			count++;
+		}
+		if (j < array2d[i].length - 1 && array2d[i][j + 1].equals(val)) {
+			count++;
+		}
+		return count;
+	}
 
-    public static int countNeighboursQueen(int i, int j, String[][] array2d, String val) {
-        int count = 0;
-        int sizeX = array2d.length;
-        int sizeY = array2d[0].length;
+	public static int countNeighboursQueen(int i, int j, String[][] array2d, String val) {
+		int count = 0;
+		int sizeX = array2d.length;
+		int sizeY = array2d[0].length;
 
-        if (i > 0) {
-            if (j > 0) {
-                for (int k = 1; i - k >= 0 && j - k >= 0; k++) {
-                    String seat = array2d[i - k][j - k];
-                    if (seat.equals("L")) {
-                        break;
-                    }
-                    if (seat.equals(val)) {
-                        count++;
-                        break;
-                    }
-                }
-            }
-            if (j < sizeY) {
-                for (int k = 1; i - k >= 0 && j + k < sizeY; k++) {
-                    String seat = array2d[i - k][j + k];
-                    if (seat.equals("L")) {
-                        break;
-                    }
-                    if (seat.equals(val)) {
-                        count++;
-                        break;
-                    }
-                }
-            }
-            for (int k = 1; i - k >= 0; k++) {
-                String seat = array2d[i - k][j];
-                if (seat.equals("L")) {
-                    break;
-                }
-                if (seat.equals(val)) {
-                    count++;
-                    break;
-                }
-            }
-        }
-        if (i < array2d.length - 1) {
-            if (j > 0) {
-                for (int k = 1; i + k < sizeX && j - k >= 0; k++) {
-                    String seat = array2d[i + k][j - k];
-                    if (seat.equals("L")) {
-                        break;
-                    }
-                    if (seat.equals(val)) {
-                        count++;
-                        break;
-                    }
-                }
-            }
-            if (j < sizeY) {
-                for (int k = 1; i + k < sizeX && j + k < sizeY; k++) {
-                    String seat = array2d[i + k][j + k];
-                    if (seat.equals("L")) {
-                        break;
-                    }
-                    if (seat.equals(val)) {
-                        count++;
-                        break;
-                    }
-                }
-            }
-            for (int k = 1; i + k < sizeX; k++) {
-                String seat = array2d[i + k][j];
-                if (seat.equals("L")) {
-                    break;
-                }
-                if (seat.equals(val)) {
-                    count++;
-                    break;
-                }
-            }
-        }
+		if (i > 0) {
+			if (j > 0) {
+				for (int k = 1; i - k >= 0 && j - k >= 0; k++) {
+					String seat = array2d[i - k][j - k];
+					if (seat.equals("L")) {
+						break;
+					}
+					if (seat.equals(val)) {
+						count++;
+						break;
+					}
+				}
+			}
+			if (j < sizeY) {
+				for (int k = 1; i - k >= 0 && j + k < sizeY; k++) {
+					String seat = array2d[i - k][j + k];
+					if (seat.equals("L")) {
+						break;
+					}
+					if (seat.equals(val)) {
+						count++;
+						break;
+					}
+				}
+			}
+			for (int k = 1; i - k >= 0; k++) {
+				String seat = array2d[i - k][j];
+				if (seat.equals("L")) {
+					break;
+				}
+				if (seat.equals(val)) {
+					count++;
+					break;
+				}
+			}
+		}
+		if (i < array2d.length - 1) {
+			if (j > 0) {
+				for (int k = 1; i + k < sizeX && j - k >= 0; k++) {
+					String seat = array2d[i + k][j - k];
+					if (seat.equals("L")) {
+						break;
+					}
+					if (seat.equals(val)) {
+						count++;
+						break;
+					}
+				}
+			}
+			if (j < sizeY) {
+				for (int k = 1; i + k < sizeX && j + k < sizeY; k++) {
+					String seat = array2d[i + k][j + k];
+					if (seat.equals("L")) {
+						break;
+					}
+					if (seat.equals(val)) {
+						count++;
+						break;
+					}
+				}
+			}
+			for (int k = 1; i + k < sizeX; k++) {
+				String seat = array2d[i + k][j];
+				if (seat.equals("L")) {
+					break;
+				}
+				if (seat.equals(val)) {
+					count++;
+					break;
+				}
+			}
+		}
 
-        if (j > 0) {
-            for (int k = 1; j - k >= 0; k++) {
-                String seat = array2d[i][j - k];
-                if (seat.equals("L")) {
-                    break;
-                }
-                if (seat.equals(val)) {
-                    count++;
-                    break;
-                }
-            }
-        }
-        if (j < sizeY) {
-            for (int k = 1; j + k < sizeY; k++) {
-                String seat = array2d[i][j + k];
-                if (seat.equals("L")) {
-                    break;
-                }
-                if (seat.equals(val)) {
-                    count++;
-                    break;
-                }
-            }
-        }
-        return count;
-    }
-
-
-    public static int countNeighbours3D(int i, int j, int k, String[][][] arr3d, String val) {
-        int count = 0;
-
-        if (i > 0) {
-            count += countNeighbours(j, k, arr3d[i - 1], val);
-            count += arr3d[i - 1][j][k].equals("#") ? 1 : 0;
-        }
-
-        if (i < arr3d.length - 1) {
-            count += countNeighbours(j, k, arr3d[i + 1], val);
-            count += arr3d[i + 1][j][k].equals("#") ? 1 : 0;
-
-        }
-        count += countNeighbours(j, k, arr3d[i], val);
-        return count;
-    }
-
-    public static int countNeighbours4D(int i, int j, int k, int l, String[][][][] arr4d, String val) {
-        int count = 0;
-
-        if (i > 0) {
-            count += countNeighbours3D(j, k, l, arr4d[i - 1], val);
-            count += arr4d[i - 1][j][k][l].equals("#") ? 1 : 0;
-        }
-
-        if (i < arr4d.length - 1) {
-            count += countNeighbours3D(j, k, l, arr4d[i + 1], val);
-            count += arr4d[i + 1][j][k][l].equals("#") ? 1 : 0;
-
-        }
-        count += countNeighbours3D(j, k, l, arr4d[i], val);
-        return count;
-    }
-
-    public static void print2D(Long[][] square) {
-        for (Long[] line : square)
-            System.out.println(Arrays.toString(line));
-        System.out.println();
-    }
-
-    public static void print2D(String[][] square) {
-        for (String[] line : square)
-            System.out.println(Arrays.toString(line));
-        System.out.println();
-    }
-
-    public static void print3D(String[][][] cube) {
-        for (String[][] square : cube) {
-            print2D(square);
-            System.out.println();
-        }
-    }
-
-    public static void print4D(String[][][][] hyperCube) {
-        for (String[][][] cube : hyperCube) {
-            print3D(cube);
-            System.out.println();
-        }
-    }
-
-    public static String[][] rotate2D(int x, String[][] square) {
-        int size = square.length;
-        String[][] rotated = new String[size][size];
-
-        switch (x){
-            case (1):
-                for (int i = 0; i < size; ++i)
-                    for (int j = 0; j < size; ++j)
-                        rotated[i][j] = square[size - j - 1][i];
-                break;
-            case (2):
-                for (int i = 0; i < size; ++i)
-                    for (int j = 0; j < size; ++j)
-                        rotated[i][j] = square[size - i - 1][size-j-1];
-                break;
-            case(3):
-                for (int i = 0; i < size; ++i)
-                    for (int j = 0; j < size; ++j)
-                        rotated[i][j] = square[j][size-i-1];
-                break;
-            default:
-                return square;
-
-        }
+		if (j > 0) {
+			for (int k = 1; j - k >= 0; k++) {
+				String seat = array2d[i][j - k];
+				if (seat.equals("L")) {
+					break;
+				}
+				if (seat.equals(val)) {
+					count++;
+					break;
+				}
+			}
+		}
+		if (j < sizeY) {
+			for (int k = 1; j + k < sizeY; k++) {
+				String seat = array2d[i][j + k];
+				if (seat.equals("L")) {
+					break;
+				}
+				if (seat.equals(val)) {
+					count++;
+					break;
+				}
+			}
+		}
+		return count;
+	}
 
 
-        return rotated;
-    }
+	public static int countNeighbours3D(int i, int j, int k, String[][][] arr3d, String val) {
+		int count = 0;
 
-    public static String[][] flip2D(String[][] square) {
-        int size = square.length;
-        String[][] flipped = new String[size][size];
+		if (i > 0) {
+			count += countNeighbours(j, k, arr3d[i - 1], val);
+			count += arr3d[i - 1][j][k].equals("#") ? 1 : 0;
+		}
 
-        for (int i = 0; i < size; ++i)
-            System.arraycopy(square[size - i - 1], 0, flipped[i], 0, size);
+		if (i < arr3d.length - 1) {
+			count += countNeighbours(j, k, arr3d[i + 1], val);
+			count += arr3d[i + 1][j][k].equals("#") ? 1 : 0;
 
-        return flipped;
-    }
+		}
+		count += countNeighbours(j, k, arr3d[i], val);
+		return count;
+	}
 
-    public static boolean sameBorderRight(String[][] left, String[][] right){
-        int size = left.length;
-        boolean matches = true;
-        for (int i = 0; i < size; i++) {
-            matches &= left[i][size - 1].equals(right[i][0]);
-        }
-        return matches;
-    }
+	public static int countNeighbours4D(int i, int j, int k, int l, String[][][][] arr4d, String val) {
+		int count = 0;
 
-    public static boolean sameBorderBottom(String[][] top, String[][] bottom){
-        return Arrays.equals(top[top.length - 1], bottom[0]);
-    }
+		if (i > 0) {
+			count += countNeighbours3D(j, k, l, arr4d[i - 1], val);
+			count += arr4d[i - 1][j][k][l].equals("#") ? 1 : 0;
+		}
 
-    public static String[][] removeBorders(String[][] square){
-        int size = square.length-2;
-        String[][] noBorders = new String[size][size];
-        for (int i = 0; i < size; i++) {
-            System.arraycopy(square[i + 1], 1, noBorders[i], 0, size);
-        }
-        return noBorders;
-    }
+		if (i < arr4d.length - 1) {
+			count += countNeighbours3D(j, k, l, arr4d[i + 1], val);
+			count += arr4d[i + 1][j][k][l].equals("#") ? 1 : 0;
+
+		}
+		count += countNeighbours3D(j, k, l, arr4d[i], val);
+		return count;
+	}
+
+	public static void print2D(Long[][] square) {
+		for (Long[] line : square)
+			System.out.println(Arrays.toString(line));
+		System.out.println();
+	}
+
+	public static void print2D(int[][] square) {
+		for (int[] line : square)
+			System.out.println(Arrays.toString(line));
+		System.out.println();
+	}
+
+	public static void print2D(String[][] square) {
+		for (String[] line : square)
+			System.out.println(Arrays.toString(line));
+		System.out.println();
+	}
+
+	public static void print3D(String[][][] cube) {
+		for (String[][] square : cube) {
+			print2D(square);
+			System.out.println();
+		}
+	}
+
+	public static void print4D(String[][][][] hyperCube) {
+		for (String[][][] cube : hyperCube) {
+			print3D(cube);
+			System.out.println();
+		}
+	}
+
+	public static String[][] rotate2D(int x, String[][] square) {
+		int size = square.length;
+		String[][] rotated = new String[size][size];
+
+		switch (x) {
+			case (1):
+				for (int i = 0; i < size; ++i)
+					for (int j = 0; j < size; ++j)
+						rotated[i][j] = square[size - j - 1][i];
+				break;
+			case (2):
+				for (int i = 0; i < size; ++i)
+					for (int j = 0; j < size; ++j)
+						rotated[i][j] = square[size - i - 1][size - j - 1];
+				break;
+			case (3):
+				for (int i = 0; i < size; ++i)
+					for (int j = 0; j < size; ++j)
+						rotated[i][j] = square[j][size - i - 1];
+				break;
+			default:
+				return square;
+
+		}
+
+
+		return rotated;
+	}
+
+	public static String[][] flip2D(String[][] square) {
+		int size = square.length;
+		String[][] flipped = new String[size][size];
+
+		for (int i = 0; i < size; ++i)
+			System.arraycopy(square[size - i - 1], 0, flipped[i], 0, size);
+
+		return flipped;
+	}
+
+	public static boolean sameBorderRight(String[][] left, String[][] right) {
+		int size = left.length;
+		boolean matches = true;
+		for (int i = 0; i < size; i++) {
+			matches &= left[i][size - 1].equals(right[i][0]);
+		}
+		return matches;
+	}
+
+	public static boolean sameBorderBottom(String[][] top, String[][] bottom) {
+		return Arrays.equals(top[top.length - 1], bottom[0]);
+	}
+
+	public static String[][] removeBorders(String[][] square) {
+		int size = square.length - 2;
+		String[][] noBorders = new String[size][size];
+		for (int i = 0; i < size; i++) {
+			System.arraycopy(square[i + 1], 1, noBorders[i], 0, size);
+		}
+		return noBorders;
+	}
 }
