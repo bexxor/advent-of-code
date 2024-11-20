4194479d74cb141ae46917509053e01dfd52e1e8
bexxor <rebekka.lutolf@gmail.com>
2024-11-20 11:28:31 +0100

    solve day 1

--- src/aoc23/Day01.java
+++ src/aoc23/Day01.java
@@ -3,40 +3,99 @@ package aoc23;
 
 import util.AoCDay;
 
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 
 public class Day01 extends AoCDay {
 
-	private List<Integer> measurements;
+	private Stream<String> calibrations;
+	private final Map<String, String> numberMap = Map.of(
+//			"zero", "0",
+			"one", "1",
+			"two", "2",
+			"three", "3",
+			"four", "4",
+			"five", "5",
+			"six", "6",
+			"seven", "7",
+			"eight", "8",
+			"nine", "9"
+	);
+	String numberPattern = String.join("|", numberMap.keySet());
+	Pattern pattern = Pattern.compile(numberPattern);
 
 	public Day01(int day) {
-		super(day, 2021, 1);
+		super(day, 2023, 2);
 		handleInput();
 	}
 
 	@Override
 	public void handleInput() {
-		this.measurements = getInputAsListOfInts();
+		this.calibrations = getInputStream();
 	}
 
 	@Override
 	public String solve1() {
-		long i = IntStream
-				.range(0, measurements.size() - 1)
-				.filter(m -> measurements.get(m) < measurements.get(m + 1))
-				.count();
-		return String.valueOf(i);
+		int sum = calibrations.mapToInt(this::toInt).sum();
+		return String.valueOf(sum);
+
 	}
 
 	@Override
 	public String solve2() {
-		this.measurements = IntStream
-				.range(0, measurements.size() - 2)
-				.map(m -> measurements.get(m) + measurements.get(m + 1) + measurements.get(m + 2))
-				.boxed().toList();
-		System.out.println();
-		return this.solve1();
+		int sum = calibrations.mapToInt(this::getValue).sum();
+		return String.valueOf(sum);
+	}
+
+	private int toInt(String s) {
+		int n = 0;
+		String z = s.replaceAll("\\D", "");
+		if (!z.isEmpty()) {
+			n = z.length() == 1 ? Integer.parseInt(z + z) : Integer.parseInt("" + z.charAt(0) + z.charAt(z.length() - 1));
+		}
+		return n;
+	}
+
+	private String toDigits(String s) {
+		if (!s.isEmpty()) {
+			Matcher matcher = pattern.matcher(s);
+			if (matcher.find()) {
+				String writtenNumber = matcher.group();
+				return numberMap.get(writtenNumber);
+			}
+		}
+		return "";
+	}
+
+	private String getFirstDigit(String s) {
+		StringBuilder z = new StringBuilder();
+		for (char c : s.toCharArray()) {
+			z.append(c);
+			if (Character.isDigit(c)) return String.valueOf(c);
+			String digit = toDigits(z.toString());
+			if (!digit.isEmpty()) return digit;
+		}
+		return "0";
+	}
+
+	private String getLastDigit(String s) {
+		StringBuilder z = new StringBuilder();
+		for (int i = s.length() - 1; i >= 0; i--) {
+			char c = s.charAt(i);
+			z.insert(0, c);
+			if (Character.isDigit(c)) return String.valueOf(c);
+			String digit = toDigits(z.toString());
+			if (!digit.isEmpty()) return digit;
+		}
+		return "0";
+	}
+
+	private int getValue(String s) {
+		String first = getFirstDigit(s);
+		String last = getLastDigit(s);
+		return Integer.parseInt(first + last);
 	}
 }
